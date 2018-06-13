4d9d886062a0b33d5c9bbf07fb6c3450809a6f5d
Evan Longwood <evanmlongwood@gmail.com>
2018-06-13 12:07:11 -0700

    Numbers and strings

---
+++ Chapter_2.js
@@ -0,0 +1,25 @@
+// Data Types and Variables.
+5;
+"Hi, I'm a string!";
+true;
+99 * 123;
+12177;
+"This is a long string".slice(0, 4);
+"This"
+true && false;
+false
+
+// Remember that semicolons mark the end of a particular
+// Javascript command or instruction(known as a statement), sort
+// of like a period at the end of a sentence.
+
+// Numbers and Operators
+
+12345 + 56789;
+22 + 33 + 44;
+1000 - 17;
+123 * 456;
+12345 / 250;
+49.38
+1234 + 57 * 3 - 31 / 4;
+console.log((15 + 9) * 2)
