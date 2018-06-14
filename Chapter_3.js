15b982455b08df219fdd245ae4bfe5221e5b6214
Evan <evanmlongwood@gmail.com>
2018-06-14 11:01:01 -0700

    Accessing an array's elements

---
+++ Chapter_3.js
@@ -0,0 +1,16 @@
+// Arrays
+
+var dinosaurs = [
+    "T-Rex",
+    "Velociraptor",
+    "Stegosaurus",
+    "Triceratops",
+    "Brachiosarus",
+    "Pteranodon",
+    "Apatosarus",
+    "Diplodocus",
+    "Compsognathus"]
+
+// accessing an array
+console.log(dinosaurs[0])
+console.log(dinosaurs[3])
