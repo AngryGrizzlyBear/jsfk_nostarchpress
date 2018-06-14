7d1c81f363fae7dea0d25e542cb8b196412f4f2f
Evan <evanmlongwood@gmail.com>
2018-06-14 11:04:44 -0700

    Adding things to an empty array.

--- Chapter_3.js
+++ Chapter_3.js
@@ -14,3 +14,11 @@ var dinosaurs = [
 // accessing an array
 console.log(dinosaurs[0])
 console.log(dinosaurs[3])
+
+// Setting or changing elements in an array
+dinosaurs[0] = "Tyrannosaurus Rex"
+console.log(dinosaurs)
+
+dinosaurs[9] = "Giant Three Toed Sloth"
+
+console.log(dinosaurs)
